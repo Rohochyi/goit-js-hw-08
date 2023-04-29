75c3175587fd42fe94915839040d1f478f3558cf
Rohochyi <oxid1109@gmail.com>
2023-04-29 15:38:06 +0300

    add

---
+++ src/js/01-gallery.js
@@ -0,0 +1,43 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const divRef = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+divRef.innerHTML = addGalleryMarkup;
+
+divRef.addEventListener('click', onImageClick);
+function onImageClick(event) {
+  event.preventDefault();
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
