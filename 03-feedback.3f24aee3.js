a07dd592bf775ababd7ebc9fddcec07e86985522
Rohochyi <94399047+Rohochyi@users.noreply.github.com>
2023-05-05 09:49:22 +0000

    Deploying to gh-pages from @ Rohochyi/goit-js-hw-08@0072ab7b2c0cf1495412f72f85f81e6b87f9d1be 🚀

---
+++ 03-feedback.3f24aee3.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");e.addEventListener("input",(()=>{console.log(e.target.value)}));
+//# sourceMappingURL=03-feedback.3f24aee3.js.map
---
+++ 03-feedback.4c52b202.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");e.addEventListener("input",(function(){console.log(e.target.value)}))}();
+//# sourceMappingURL=03-feedback.4c52b202.js.map
